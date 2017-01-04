0c53fbb36fa572d21648562bf5f0659dc3354c60
pixhub <nunes.christophe@gmail.com>
2017-01-04 18:28:35 +0100

    Create Desktop2ffmpeg.sh
    A script shell to create videos of your Desktop.

---
+++ desktop2ffmpeg.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+FFMPEG ="/usr/bin/ffmpeg"
+
+echo "Desktop Size :"
+read INRES
+
+echo "Framerate :"
+read FPS
+
+echo "File name :"
+read FILE
+
+$FFMPEG -f x11grab -s $INRES -r $FPS -i :0.0 \
+-f alsa -i pulse -vcodec libx264 \
+/home/account_name/Videos/$FILE.mpg
