4a150019158d557726df2df1de2bb0ac1224f34d
pixhub <nunes.christophe@gmail.com>
2017-01-09 16:39:36 +0100

    Update desktop2ffmpeg.sh

--- desktop2ffmpeg.sh
+++ desktop2ffmpeg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-s <screen resolution>] [-r <FPS> ] [-o <path and file name to save> ]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-s <screen resolution>] [-r <FPS> ] [-o <path and file name to save> ] [-h <help> ]" 1>&2; exit 1; }
 
-while getopts ":s:r:o:" o; do
+while getopts ":s:r:o:h" o; do
     case "${o}" in
         s)
             RES=${OPTARG}
@@ -13,6 +13,21 @@ while getopts ":s:r:o:" o; do
 	o)
 	    FILE=${OPTARG}
 	    ;;
+	h)  
+	    echo ""
+	    echo "Welcome on the desktop2ffmpeg helper"
+	    echo ""
+	    echo "Options :"
+	    echo ""
+	    echo "-s is your screen size, you must inquire this option with the correct size of your screen (ex : 1920x1080)"
+	    echo ""
+	    echo "-r is the frame rate. (Default framerate = 30)"
+	    echo ""
+	    echo "-o is the file saving path. (Default = ~/video1.mpg)"
+	    echo ""
+	    echo "For -o do not inquire the file extension. It is already inquired as .mpg"
+	    echo ""
+	    ;;
         *)
             usage
             ;;
@@ -20,16 +35,21 @@ while getopts ":s:r:o:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "$RES" ] || [ -z "$FPS" ]; 
+if [ -z "$RES" ]; 
   then usage
 fi
-if [ -z "$FILE" ]; 
-  then FILE=~/video1
+
+if [ -z "$FPS" ];
+  then FPS=30
 fi
 
-echo "Chosen resolution : $RES"
-echo "Chosen frame rate : $FPS"
-echo "Save file path : $FILE"
+if [ -z "$FILE" ];
+  then  FILE=~/video1 
+fi
+
+echo "Screen resolution = $RES"
+echo "Frame rate = $FPS fps"
+echo "Saved file path = $FILE.mpg"
 
 FFMPEG=/usr/bin/ffmpeg
 
