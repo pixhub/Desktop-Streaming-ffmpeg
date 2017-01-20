b7730503db786b2cbc3ac4af5072970da0a5f7eb
pixhub <nunes.christophe@gmail.com>
2017-01-20 18:38:27 +0100

    ffmpeg2nginx launch script
    A launch script with options

--- ffmpeg2nginx.sh
+++ ffmpeg2nginx.sh
@@ -1,9 +1,56 @@
-#! /bin/bash
+while getopts ":s:r:o:i:h" o; do
+    case "${o}" in
+        s)
+            INRES=${OPTARG}
+            ;;
+	r)
+	    FPS=${OPTARG}
+	    ;;
+	o)
+	    OUTRES=${OPTARG}
+	    ;;
+	i)
+	    IP=${OPTARG}
+	    ;;
+	h)  
+	    echo ""
+	    echo "Welcome on the desktop2ffmpeg helper"
+	    echo ""
+	    echo "Options :"
+	    echo ""
+	    echo "-s is your screen size, you must inquire this option with your actual screen size (ex : 1920x1080)"
+	    echo ""
+	    echo "-r is the frame rate. (Default framerate = 30)"
+	    echo ""
+	    echo "-o is the stream resolution (output resolution)"
+	    echo ""
+	    echo "-i is the server IP"
+	    echo ""
+	    ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$INRES" ] || [ -z "$OUTRES" ] || [ -z "$IP" ]; 
+  then usage
+fi
+
+if [ -z "$FPS" ];
+  then FPS=30
+fi
+
+echo "Screen resolution = $RES"
+echo "Frame rate = $FPS fps"
+echo "Stream resolution = $OUTRES.mpg"
+echo "Streaming path = rtmp://$IP:1935/live"
 
 FFMPEG="/usr/bin/ffmpeg"
 
-$FFMPEG -f x11grab -s 1920x1080 -r 30 -i :0.0 \
+$FFMPEG -f x11grab -s $INRES -r $FPS -i :0.0 \
 -f alsa -ac 2 -i pulse -vcodec libx264 \
--s 1280x720 -acodec libmp3lame \
+-s $OUTRES -strict -2 -acodec aac \
 -ab 128k -ar 44100 -threads 0 \
--f flv "rtmp://IP_nginx_server/live"
+-f flv "rtmp://$IP:1935/live"
